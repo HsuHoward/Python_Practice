33114d5325d57a9100b4af20b91b8344d53ca21f
HsuHoward <43441110+HsuHoward@users.noreply.github.com>
2018-10-18 14:17:13 +0800

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 14:58:03 2018
+
+@author: howardhsu
+"""
+
+#ex 13
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
